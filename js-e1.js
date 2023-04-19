2e96ee8cec1cb2fc9b9165a5632561531529f718
Ender Romero <endjr1976@gmail.com>
2023-04-18 21:23:11 -0300

    Ejercicios 5 y 6 done

--- js-e1.js
+++ js-e1.js
@@ -6,9 +6,6 @@ function parOImpar(num) {
     : console.log(`El número ${num} es impar`);
 }
 
-parOImpar(4);
-parOImpar(7);
-
 // 2- Crear una función que reciba dos números por parámetro e indique en consola que número es mayor, y si ninguno lo es, indicar por consola que son iguales.
 
 function mayorMenorOIgual(num1, num2) {
@@ -21,10 +18,6 @@ function mayorMenorOIgual(num1, num2) {
   }
 }
 
-mayorMenorOIgual(2, 5);
-mayorMenorOIgual(5, 5);
-mayorMenorOIgual(8, 5);
-
 // 3- Crear una función que reciba un número por parámetro e indique en consola si ese número es múltiplo de 5.
 
 function multiploDe5(num) {
@@ -32,9 +25,6 @@ function multiploDe5(num) {
     ? console.log(`El número ${num} es múltiplo de 5`)
     : console.log(`El número ${num} no es múltiplo de 5`);
 }
-
-multiploDe5(15);
-multiploDe5(17);
 
 // 4- Crear una función que reciba un número por parámetro e imprima por consola todos los números desde el 0 hasta llegar a ese número.
 
@@ -44,12 +34,21 @@ function conteo(num) {
   }
 }
 
-conteo(5)
-
 // 5 - Crear una función que reciba una palabra y un número por parámetro e imprima por consola  esa palabra la cantidad correspondiente al número indicado.
 
-
+function repetirPalabra(word, num) {
+  for (let i = 0; i < num; i++) {
+    console.log(word);
+  }
+}
 
 // 6 - Crear una función que reciba un array por parámetro e imprima por consola todos los valores de ese array.
+
+function imprimirArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
+}
+
 // 7 - Crear una función que reciba un array con 10 números e imprima por consola todos los valores de ese array, menos el que se encuentre en la 5ta posición del mismo. Ayuda: Recuerden que el primer índice de un array es "0".
 // 8 - Crea una función que reciba un array de números y un número por parámetro e imprima por consola cada número del array multiplicado por el número pasado por parámetro.
